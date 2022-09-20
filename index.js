03a1453d119391a0c9572d10b2d95076113a72eb
Rahul Jain <108295222+rahulljainn@users.noreply.github.com>
2022-09-20 18:44:19 +0000

    update-fixlowercase

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var username = readlineSync.question("what is your name..?")
 console.log("hello " + username + " Let's have a fun quiz about bollywood..?")
 function play(question, answer) {
   var useranswer = readlineSync.question(question)
-  if (answer === useranswer) {
+  if (answer.toLowerCase() == useranswer.toLowerCase()) {
     console.log("right!")
     score = score + 1;
   }
